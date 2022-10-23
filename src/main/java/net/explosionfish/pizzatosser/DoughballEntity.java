e3c528fb5694aacbabd0622a7a4767deb20ae085
bunnycou <noahjcou@gmail.com>
2022-10-22 20:10:34 -0400

    Modified how the pizza dough functions when it hits the ground

--- src/main/java/net/explosionfish/pizzatosser/DoughballEntity.java
+++ src/main/java/net/explosionfish/pizzatosser/DoughballEntity.java
@@ -9,9 +9,11 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.thrown.ThrownItemEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.particle.ItemStackParticleEffect;
 import net.minecraft.particle.ParticleEffect;
 import net.minecraft.particle.ParticleTypes;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.world.World;
@@ -38,7 +40,7 @@ public class DoughballEntity extends ThrownItemEntity {
     @Environment(EnvType.CLIENT)
     private ParticleEffect getParticleParameters() {
         ItemStack itemStack = this.getItem();
-        return itemStack.isEmpty() ? ParticleTypes.ITEM_SNOWBALL : new ItemStackParticleEffect(ParticleTypes.ITEM, itemStack);
+        return itemStack.isEmpty() ? ParticleTypes.CRIT : new ItemStackParticleEffect(ParticleTypes.ITEM, itemStack);
     }
 
     @Environment(EnvType.CLIENT)
@@ -66,6 +68,11 @@ public class DoughballEntity extends ThrownItemEntity {
         if (!this.world.isClient) {
             this.world.sendEntityStatus(this, (byte) 3);
             this.discard();
+
+            if (hitResult.getType() != HitResult.Type.ENTITY) {
+                this.dropStack(new ItemStack(Items.DIRT));
+                this.playSound(SoundEvents.ENTITY_CREEPER_PRIMED, 0.8f, 0.5f);
+            }
         }
     }
 }
